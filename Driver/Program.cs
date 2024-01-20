93df41e82334f9cab7d43a3b6e52f37e67db01f3
Marthinus Richter <richter.ms@outlook.com>
2024-01-20 22:29:08 +0200

    Add project files.

---
+++ Driver/Program.cs
@@ -0,0 +1,129 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PipelineChunker;
+using System.Collections;
+using System.Data;
+using System.Diagnostics;
+
+
+static class Program {
+    static void Main(string[] args) {
+        var pipe = new Pipeline();
+
+        var TestConduit1Open = false;
+        var TestConduit1Channeling = false;
+        do {
+            for (int i = 0; i < 100; i++) {
+                pipe.Channel<TestConduit1>(
+                    Origin: () => {
+                        return Pipelined1(i, pipe);
+                    },
+                    Operation: (conduit) => {
+                        Debug.WriteLine(conduit);
+                        // invoked when all enumerators terminate.
+                    });
+            }
+            pipe.Flush<TestConduit1>();
+            pipe.GetChannelState<TestConduit1>(ref TestConduit1Open, ref TestConduit1Channeling);
+        } while (pipe.IsOpen);
+    }
+
+
+    internal class TestConduit1 : IConduit {
+        int _id;
+        int IConduit.Id => _id;
+        Pipeline.IChannelState _channelItem;
+        public Pipeline.IChannelState channelItem1 => _channelItem;
+        public IEnumerator<TestConduit1> GetEnumerator() {
+            throw new NotImplementedException();
+        }
+        IEnumerator IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
+        }
+        void IConduit.Initialize(IPipeline conduitOwner) {
+            TestConduit1 self = this;
+            conduitOwner.Bind(ref self, ref _id, ref _channelItem);
+        }
+        public class Phase1 : IPhase {
+            public DataTable DataTable {
+                get {
+                    var table = new DataTable();
+                    table.Columns.Add("SomeParameter");
+                    return table;
+                }
+            }
+
+            public void Execute(DataTable table) {
+                throw new NotImplementedException();
+            }
+        }
+        public class Phase2 : IPhase {
+            public DataTable DataTable => throw new NotImplementedException();
+
+            public void Execute(DataTable table) {
+                throw new NotImplementedException();
+            }
+        }
+    }
+    internal class TestConduit2 : IConduit {
+        int _id;
+        int IConduit.Id => _id;
+        Pipeline.IChannelState _channelItem;
+        public Pipeline.IChannelState channelItem1 => _channelItem;
+        public IEnumerator<TestConduit2> GetEnumerator() {
+            throw new NotImplementedException();
+        }
+        IEnumerator IEnumerable.GetEnumerator() {
+            throw new NotImplementedException();
+        }
+        void IConduit.Initialize(IPipeline conduitOwner) {
+            TestConduit2 self = this;
+            conduitOwner.Bind(ref self, ref _id, ref _channelItem);
+        }
+    }
+
+    static IEnumerable<TestConduit1> Pipelined1(int i, Pipeline owner) {
+        var conduit = new TestConduit1();
+        // first yield will initialize the conduit.
+        yield return conduit;
+        Debug.WriteLine($"fist {i}");
+        conduit.channelItem1.Chunk<TestConduit1.Phase1>(
+            conduit,
+            (DataRow row) => {
+                row["SomeParameter"] = 1;
+            },
+            (DataTable table) => {
+
+            });
+        yield return conduit;
+
+        conduit.channelItem1.Chunk<TestConduit1.Phase1>(
+            conduit,
+            (DataRow row) => {
+                row["SomeParameter"] = 1;
+            },
+            (DataTable table) => {
+
+            });
+        Debug.WriteLine($"second {i}");
+        yield return conduit;
+
+        conduit.channelItem1.Chunk<TestConduit1.Phase1>(
+            conduit,
+            (DataRow row) => {
+                row["SomeParameter"] = 1;
+            },
+            (DataTable table) => {
+
+            });
+        Debug.WriteLine($"third {i}");
+        yield return conduit;
+    }
+    static IEnumerable<TestConduit2> Pipelined2(int i, Pipeline owner) {
+        var conduit = new TestConduit2();
+        // first yield will initialize the conduit.
+        yield return conduit;
+
+        yield return conduit;
+    }
+
+}
---
+++ PipelineChunker/Scratchpad.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Text;
+using static System.Collections.Specialized.BitVector32;
+using System.Linq;
+using System.Diagnostics;
+
+namespace PipelineChunker {
+        public interface IPipeline {
+            void Bind<ConduitT>(ref ConduitT conduitT, ref int id, ref Pipeline.IChannelState channelItem);
+            void GetChannelState<ConduitT>(ref bool IsOpen, ref bool IsChanneling);
+            bool IsOpen { get; }
+        }
+        public interface IConduit : IEnumerable {
+            void Initialize(IPipeline conduitOwner);
+            int Id { get; }
+            Pipeline.IChannelState channelItem1 { get; }
+        }
+        public interface IPhase {
+            DataTable DataTable { get; }
+            void Execute(DataTable table);
+        }
+
+        public class Pipeline : IPipeline {
+
+            private class ChannelState : IChannelState {
+                public bool IsChanneling;
+                public bool IsOpen;
+                public List<ChannelItem> list;
+
+                public void Chunk<PhaseT>(IConduit conduit, Action<DataRow> value, Action<DataTable> value1) {
+                }
+            }
+            public interface IChannelState {
+                void Chunk<PhaseT>(IConduit conduit, Action<DataRow> value, Action<DataTable> value1);
+            }
+            private struct ChannelItem {
+                public Action<IConduit> Operation;
+                public IEnumerator<IConduit> Enumerator;
+            }
+            private Dictionary<Type, ChannelState> _conduitMap = new Dictionary<Type, ChannelState>();
+
+            public bool IsOpen {
+                get {
+                    return !_conduitMap.Values.All(x => !x.IsOpen);
+                }
+            }
+
+            public void Bind<ConduitT>(ref ConduitT conduitT, ref int id, ref Pipeline.IChannelState channelItem) {
+                var conduitType = typeof(ConduitT);
+                if (!_conduitMap.TryGetValue(conduitType, out var state)) {
+                    throw new Exception($"Bind<{typeof(ConduitT).FullName}>() must only be called from {typeof(IConduit).FullName}'s {nameof(IConduit.Initialize)} method");
+                }
+                channelItem = state;
+                id = state.list.Count - 1;
+              }
+
+            public void Channel<IConduitT>(Func<IEnumerable<IConduit>> Origin, Action<IConduit> Operation) {
+                var conduitType = typeof(IConduitT);
+                if (!_conduitMap.TryGetValue(conduitType, out var state)) {
+                    _conduitMap[conduitType] = state = new ChannelState();
+                    state.list = new List<ChannelItem>();
+                    state.IsOpen = false;
+                    var enumerator = 
+                    state.IsChanneling = false;
+                }
+                state.list.Add(new ChannelItem() {
+                    Operation = Operation,
+                    Enumerator = Origin().GetEnumerator()
+                });
+            }
+
+            public void Clear() {
+                //throw new NotImplementedException();
+            }
+
+            public IEnumerable<IEnumerator<IConduit>> Flush<IConduitT>() {
+                var conduitType = typeof(IConduitT);
+                if (_conduitMap.TryGetValue(conduitType, out var state)) {
+                    bool wasChanneling = state.IsChanneling;
+                    if (!state.IsOpen) {
+                        foreach (var item in state.list) {
+                            if (!item.Enumerator.MoveNext()) {
+                                throw new Exception("Channel must receive a conduit that yields a non null value");
+                            }
+                            item.Enumerator.Current.Initialize(this);
+                        }
+                        state.IsChanneling = true;
+                    }
+                    while (state.IsChanneling) {
+                        foreach (var item in state.list) {
+                            var end = item.Enumerator.MoveNext();
+                            state.IsChanneling &= !end;
+                        }
+                        state.IsChanneling = !state.IsChanneling;
+                    }
+                    // all iterators completed, run the operations for all conduits, but only once.
+                    foreach (var item in state.list) {
+                        item.Operation(item.Enumerator.Current);
+
+                    }
+                }
+                return state.list.Select(x => x.Enumerator);
+            }
+
+            public void GetChannelState<ConduitT>(ref bool IsOpen, ref bool IsChanneling) {
+                if (_conduitMap.TryGetValue(typeof(ConduitT), out var item)) {
+                    IsOpen = item.IsOpen;
+                    IsChanneling = item.IsChanneling;
+                } else {
+                    IsOpen = false;
+                    IsChanneling = false;
+                }
+            }
+
+        }
+}
