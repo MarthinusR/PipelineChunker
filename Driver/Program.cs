c19edb9faafc3c7d5a72960e80790ee845c5a455
Marthinus Richter <richter.ms@outlook.com>
2024-01-21 14:15:13 +0200

    Dumping progress. - public IPhase phase should be implemented as a list instead.

--- Driver/Program.cs
+++ Driver/Program.cs
@@ -3,28 +3,185 @@ using PipelineChunker;
 using System.Collections;
 using System.Data;
 using System.Diagnostics;
-
+using Microsoft.Data.SqlClient;
+using System.Data.Common;
+using System.Data.SqlTypes;
 
 static class Program {
+    static bool IsInitExecFlattenedStoreProcAsDataSetBatcher = false;
+    static Dictionary<string, Func<DataTable, DataSet>> preparedMap = new Dictionary<string, Func<DataTable, DataSet>>();
+    static DataSet ExecFlattenedStoreProcAsDataSetBatcher(SqlCommand cmd, string storeProc, DataTable parameterTable) {
+        DataSet dataSet;
+        if (!IsInitExecFlattenedStoreProcAsDataSetBatcher) {
+            IsInitExecFlattenedStoreProcAsDataSetBatcher = true;
+            cmd.CommandText = $"""                        
+            create or alter procedure usp_FlattenedStoreProcAsDataSetBatcher
+            	@proc varchar(255)
+            as begin
+            	declare @unique varchar(255) = 'Batched_c82473d65be546d8b965897a6ac039c0_'
+            	declare @outProcName varchar(255) = 'usp_' + @unique + @proc
+            	declare @inputTypeName varchar(255) = @unique + @proc + 'UserType'
+
+            	declare @tblInfo as table(
+            		name varchar(255),
+            		type varchar(255)
+            	)
+            	-- how-to-get-stored-procedure-parameters-details https://stackoverflow.com/a/41330791
+            	insert into @tblInfo (name, type) select parameters.name, t.name from sys.parameters 
+            		inner join sys.procedures p on parameters.object_id = p.object_id
+            		inner join sys.types t on parameters.system_type_id = t.system_type_id AND parameters.user_type_id = t.user_type_id
+            		where p.name = 'usp_Example'
+
+            	DECLARE @Parameters VARCHAR(8000) 
+            	DECLARE @AssignedParameters VARCHAR(8000) 
+            	DECLARE @ProcedureParameters VARCHAR(8000) 
+            	DECLARE @TableParameters VARCHAR(8000) 
+            	SELECT @TableParameters = COALESCE(@TableParameters + ', ', '') + REPLACE(name, '@', '') + ' ' + 
+            		case
+            			when type = 'varchar' then (type + '(MAX)')
+            			else type
+            		end
+            	from @tblInfo
+            	SELECT @ProcedureParameters = COALESCE(@ProcedureParameters + ' declare ', 'declare ') + name + ' ' + 
+            		case
+            			when type = 'varchar' then (type + '(MAX)')
+            			else type
+            		end
+            	from @tblInfo
+            	SELECT @Parameters = COALESCE(@Parameters + ', ', '') + name
+            	from @tblInfo
+            	SELECT @AssignedParameters = COALESCE(@AssignedParameters + ', ', '') + name + '=' + name
+            	from @tblInfo
+
+            	exec(
+            	'if object_id(''' + @outProcName + ''') is not null
+            	begin
+            		drop procedure ' + @outProcName + '
+            	end
+            	')
+            	exec(
+            	'if type_id(''' + @inputTypeName + ''') is not null
+            	begin
+            		drop type ' + @inputTypeName + '
+            	end
+            	create type ' + @inputTypeName + ' as table(
+            		' + @TableParameters + '
+            	)')
+
+            	exec(
+            	'create or alter procedure ' + @outProcName + '
+            		@input ' + @inputTypeName + ' readonly
+            	as begin
+            		' + @ProcedureParameters + '
+            		declare #cursor cursor local dynamic forward_only read_only for (select * from @input)
+            		OPEN #cursor
+            		do_loop:
+            			fetch next from #cursor into ' + @Parameters + '
+            			if @@FETCH_STATUS <> 0
+            				goto exit_do_loop
+            			begin try
+            				exec usp_Example ' + @AssignedParameters + '
+            			end try begin catch
+            				(select ERROR_MESSAGE(), ERROR_SEVERITY(), ERROR_STATE())
+            			end catch
+            		goto do_loop
+            		exit_do_loop:
+            	end')
+            end            
+            """;
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Clear();
+            cmd.ExecuteNonQuery();
+        }
+
+        if(!preparedMap.TryGetValue(storeProc, out var process)) {
+            cmd.CommandText = $"exec usp_FlattenedStoreProcAsDataSetBatcher '{storeProc}'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Parameters.Clear();
+            cmd.ExecuteNonQuery();
+            preparedMap[storeProc] = process = (dataTable) => {
+                cmd.CommandText = $"usp_Batched_c82473d65be546d8b965897a6ac039c0_{storeProc}";
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Clear();
+                SqlParameter parameter = cmd.Parameters.AddWithValue("@input", dataTable);
+                parameter.SqlDbType = SqlDbType.Structured;
+                parameter.TypeName = $"Batched_c82473d65be546d8b965897a6ac039c0_{storeProc}UserType";
+                DataSet set = new DataSet();
+                new SqlDataAdapter(cmd).Fill(set);
+                return set;
+            };
+        }
+        return process(parameterTable);
+    }
     static void Main(string[] args) {
         var pipe = new Pipeline();
 
-        var TestConduit1Open = false;
-        var TestConduit1Channeling = false;
-        do {
-            for (int i = 0; i < 100; i++) {
-                pipe.Channel<TestConduit1>(
-                    Origin: () => {
-                        return Pipelined1(i, pipe);
-                    },
-                    Operation: (conduit) => {
-                        Debug.WriteLine(conduit);
-                        // invoked when all enumerators terminate.
-                    });
+        var connectionStringBuilder = new SqlConnectionStringBuilder();
+        connectionStringBuilder.ConnectionString = "Server=localhost\\SQLEXPRESS;Database=PipelineChunker;Trusted_Connection=True;Encrypt=False;";
+        using (SqlConnection conn = new SqlConnection(connectionStringBuilder.ConnectionString)) {
+            conn.Open();
+            string sql = $"""
+                if object_id(N'tblExample', N'U') is null
+                begin
+                    create table tblExample (
+                        id int
+                    )
+                    select 1
+                end
+                """;
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            using (SqlDataReader reader = cmd.ExecuteReader()) {
+                while (reader.Read()) {
+                    var result = reader.GetInt32(0);
+                    if (result == 1) {
+                        Debug.WriteLine(" --- Created tblExample");
+                    }
+                }
             }
-            pipe.Flush<TestConduit1>();
-            pipe.GetChannelState<TestConduit1>(ref TestConduit1Open, ref TestConduit1Channeling);
-        } while (pipe.IsOpen);
+            sql = $"""
+                if object_id(N'usp_Example') is null
+                begin
+                	exec('create or alter procedure usp_Example
+                		@a int,
+                		@b int
+                	as begin
+                		(select (@a + @b))
+                	end')
+                	select 1
+                end
+                """;
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            using (SqlDataReader reader = cmd.ExecuteReader()) {
+                while (reader.Read()) {
+                    var result = reader.GetInt32(0);
+                    if (result == 1) {
+                        Debug.WriteLine(" --- Created usp_Example");
+                    }
+                }
+            }
+
+        }
+
+        using (SqlConnection conn = new SqlConnection(connectionStringBuilder.ConnectionString))
+        using (SqlCommand cmd = conn.CreateCommand()) {
+            conn.Open();
+            var TestConduit1Open = false;
+            var TestConduit1Channeling = false;
+            do {
+                for (int i = 0; i < 100; i++) {
+                    pipe.Channel<TestConduit1>(
+                        Origin: () => {
+                            return Pipelined1(cmd, i, pipe);
+                        },
+                        Operation: (conduit) => {
+                            Debug.WriteLine(conduit);
+                            // invoked when all enumerators terminate.
+                        });
+                }
+                pipe.Flush<TestConduit1>();
+                pipe.GetChannelState<TestConduit1>(ref TestConduit1Open, ref TestConduit1Channeling);
+            } while (pipe.IsOpen);
+        }
     }
 
 
@@ -32,7 +189,7 @@ static class Program {
         int _id;
         int IConduit.Id => _id;
         Pipeline.IChannelState _channelItem;
-        public Pipeline.IChannelState channelItem1 => _channelItem;
+        public Pipeline.IChannelState channelItem => _channelItem;
         public IEnumerator<TestConduit1> GetEnumerator() {
             throw new NotImplementedException();
         }
@@ -43,24 +200,42 @@ static class Program {
             TestConduit1 self = this;
             conduitOwner.Bind(ref self, ref _id, ref _channelItem);
         }
-        public class Phase1 : IPhase {
-            public DataTable DataTable {
+        public class Phase1 : Phase {
+            public SqlCommand cmd;
+            public int value;
+            public Phase1() { }
+            override public IEnumerable<KeyValuePair<string, DataTable>> parameterTables {
                 get {
                     var table = new DataTable();
-                    table.Columns.Add("SomeParameter");
-                    return table;
+                    table.Columns.Add("@a");
+                    table.Columns.Add("@b");
+                    var theParameterTables = new KeyValuePair<string, DataTable>[]{
+                        new KeyValuePair<string, DataTable>("@input", table)
+                    };
+                    return theParameterTables;
                 }
             }
 
-            public void Execute(DataTable table) {
-                throw new NotImplementedException();
+            override public DataSet Execute(IEnumerable<KeyValuePair<String, DataTable>> parameterTables) {
+                return ExecFlattenedStoreProcAsDataSetBatcher(cmd, "usp_Example", parameterTables.First().Value);
             }
         }
-        public class Phase2 : IPhase {
-            public DataTable DataTable => throw new NotImplementedException();
+        public class Phase2 : Phase {
+            public SqlCommand cmd;
+            override public IEnumerable<KeyValuePair<string, DataTable>> parameterTables {
+                get {
+                    var table = new DataTable();
+                    table.Columns.Add("@a");
+                    table.Columns.Add("@b");
+                    var theParameterTables = new KeyValuePair<string, DataTable>[]{
+                        new KeyValuePair<string, DataTable>("@input", table)
+                    };
+                    return theParameterTables;
+                }
+            }
 
-            public void Execute(DataTable table) {
-                throw new NotImplementedException();
+            public override DataSet Execute(IEnumerable<KeyValuePair<string, DataTable>> parameterTables) {
+                return ExecFlattenedStoreProcAsDataSetBatcher(cmd, "usp_Example", parameterTables.First().Value);
             }
         }
     }
@@ -68,7 +243,7 @@ static class Program {
         int _id;
         int IConduit.Id => _id;
         Pipeline.IChannelState _channelItem;
-        public Pipeline.IChannelState channelItem1 => _channelItem;
+        public Pipeline.IChannelState channelItem => _channelItem;
         public IEnumerator<TestConduit2> GetEnumerator() {
             throw new NotImplementedException();
         }
@@ -81,38 +256,45 @@ static class Program {
         }
     }
 
-    static IEnumerable<TestConduit1> Pipelined1(int i, Pipeline owner) {
+    static IEnumerable<TestConduit1> Pipelined1(SqlCommand cmd, int i, Pipeline owner) {
         var conduit = new TestConduit1();
         // first yield will initialize the conduit.
         yield return conduit;
+        int someValue = 0;
         Debug.WriteLine($"fist {i}");
-        conduit.channelItem1.Chunk<TestConduit1.Phase1>(
+        var phase1 = conduit.channelItem.Chunk<TestConduit1.Phase1>(
             conduit,
             (DataRow row) => {
-                row["SomeParameter"] = 1;
+                row["@a"] = i;
+                row["@b"] = i;
             },
-            (DataTable table) => {
-
+            (DataTable table, bool isError) => {
+                someValue = (int)table.Rows[0].ItemArray[0];
+                Debug.WriteLine($"someValue {someValue}");
             });
+        phase1.cmd = cmd;
         yield return conduit;
 
-        conduit.channelItem1.Chunk<TestConduit1.Phase1>(
+        Debug.WriteLine($"Phase1-computed: {someValue}  -- {i}");
+
+        conduit.channelItem.Chunk<TestConduit1.Phase2>(
             conduit,
             (DataRow row) => {
-                row["SomeParameter"] = 1;
+                row["@a"] = 10 * i;
+                row["@b"] = 10 * i;
             },
-            (DataTable table) => {
-
+            (DataTable table, bool isError) => {
+                Debug.WriteLine(table.Rows[0].ItemArray[0]);
             });
         Debug.WriteLine($"second {i}");
         yield return conduit;
 
-        conduit.channelItem1.Chunk<TestConduit1.Phase1>(
+        conduit.channelItem.Chunk<TestConduit1.Phase1>(
             conduit,
             (DataRow row) => {
                 row["SomeParameter"] = 1;
             },
-            (DataTable table) => {
+            (DataTable table, bool isError) => {
 
             });
         Debug.WriteLine($"third {i}");
--- PipelineChunker/Scratchpad.cs
+++ PipelineChunker/Scratchpad.cs
@@ -9,112 +9,172 @@ using System.Linq;
 using System.Diagnostics;
 
 namespace PipelineChunker {
-        public interface IPipeline {
-            void Bind<ConduitT>(ref ConduitT conduitT, ref int id, ref Pipeline.IChannelState channelItem);
-            void GetChannelState<ConduitT>(ref bool IsOpen, ref bool IsChanneling);
-            bool IsOpen { get; }
+    public interface IPipeline {
+        void Bind<ConduitT>(ref ConduitT conduitT, ref int id, ref Pipeline.IChannelState channelItem);
+        void GetChannelState<ConduitT>(ref bool IsOpen, ref bool IsChanneling);
+        bool IsOpen { get; }
+    }
+    public interface IConduit : IEnumerable {
+        void Initialize(IPipeline conduitOwner);
+        int Id { get; }
+        Pipeline.IChannelState channelItem { get; }
+    }
+    public interface IPhase {
+        void Init(Action<DataTable, bool> operation);
+        IEnumerable<KeyValuePair<String, DataTable>> parameterTables { get; }
+        DataSet Execute(IEnumerable<KeyValuePair<String, DataTable>> parameterTables);
+        Action<DataTable, bool> Operation { get; }
+    }
+    public abstract class Phase : IPhase {
+        public abstract IEnumerable<KeyValuePair<string, DataTable>> parameterTables { get; }
+        public Action<DataTable, bool> Operation { get; private set; }
+
+        public abstract DataSet Execute(IEnumerable<KeyValuePair<string, DataTable>> parameterTables);
+        public void Init(Action<DataTable, bool> operation) {
+            Operation = operation;
         }
-        public interface IConduit : IEnumerable {
-            void Initialize(IPipeline conduitOwner);
-            int Id { get; }
-            Pipeline.IChannelState channelItem1 { get; }
-        }
-        public interface IPhase {
-            DataTable DataTable { get; }
-            void Execute(DataTable table);
-        }
+    }
 
-        public class Pipeline : IPipeline {
+    public class Pipeline : IPipeline {
+        private class ChannelState : IChannelState {
+            public bool IsChanneling;
+            public bool IsOpen;
+            public List<ChannelItem> list;
 
-            private class ChannelState : IChannelState {
-                public bool IsChanneling;
-                public bool IsOpen;
-                public List<ChannelItem> list;
+            public IPhase phase;
 
-                public void Chunk<PhaseT>(IConduit conduit, Action<DataRow> value, Action<DataTable> value1) {
-                }
-            }
-            public interface IChannelState {
-                void Chunk<PhaseT>(IConduit conduit, Action<DataRow> value, Action<DataTable> value1);
-            }
-            private struct ChannelItem {
-                public Action<IConduit> Operation;
-                public IEnumerator<IConduit> Enumerator;
-            }
-            private Dictionary<Type, ChannelState> _conduitMap = new Dictionary<Type, ChannelState>();
+            //TODO: should this not just be a list?
+            public Dictionary<Type, List<IPhase>> phaseMap = new Dictionary<Type, List<IPhase>>();
 
-            public bool IsOpen {
-                get {
-                    return !_conduitMap.Values.All(x => !x.IsOpen);
-                }
-            }
+            public IEnumerable<KeyValuePair<String, DataTable>> parameterTables;
 
-            public void Bind<ConduitT>(ref ConduitT conduitT, ref int id, ref Pipeline.IChannelState channelItem) {
-                var conduitType = typeof(ConduitT);
-                if (!_conduitMap.TryGetValue(conduitType, out var state)) {
-                    throw new Exception($"Bind<{typeof(ConduitT).FullName}>() must only be called from {typeof(IConduit).FullName}'s {nameof(IConduit.Initialize)} method");
-                }
-                channelItem = state;
-                id = state.list.Count - 1;
-              }
-
-            public void Channel<IConduitT>(Func<IEnumerable<IConduit>> Origin, Action<IConduit> Operation) {
-                var conduitType = typeof(IConduitT);
-                if (!_conduitMap.TryGetValue(conduitType, out var state)) {
-                    _conduitMap[conduitType] = state = new ChannelState();
-                    state.list = new List<ChannelItem>();
-                    state.IsOpen = false;
-                    var enumerator = 
-                    state.IsChanneling = false;
-                }
-                state.list.Add(new ChannelItem() {
-                    Operation = Operation,
-                    Enumerator = Origin().GetEnumerator()
-                });
-            }
-
-            public void Clear() {
-                //throw new NotImplementedException();
-            }
-
-            public IEnumerable<IEnumerator<IConduit>> Flush<IConduitT>() {
-                var conduitType = typeof(IConduitT);
-                if (_conduitMap.TryGetValue(conduitType, out var state)) {
-                    bool wasChanneling = state.IsChanneling;
-                    if (!state.IsOpen) {
-                        foreach (var item in state.list) {
-                            if (!item.Enumerator.MoveNext()) {
-                                throw new Exception("Channel must receive a conduit that yields a non null value");
-                            }
-                            item.Enumerator.Current.Initialize(this);
-                        }
-                        state.IsChanneling = true;
+            public string GetParameterSigniture() {
+                StringBuilder sbForProcParametersHash = new StringBuilder();
+                foreach (var pair in parameterTables.OrderBy(x => x.Key)) {
+                    sbForProcParametersHash.Append(pair.Key);
+                    foreach (var col in pair.Value.Columns.Cast<DataColumn>().OrderBy(x => x.ColumnName)) {
+                        sbForProcParametersHash.Append(col.ColumnName);
                     }
-                    while (state.IsChanneling) {
-                        foreach (var item in state.list) {
-                            var end = item.Enumerator.MoveNext();
-                            state.IsChanneling &= !end;
-                        }
-                        state.IsChanneling = !state.IsChanneling;
+                }
+                return sbForProcParametersHash.ToString();
+            }
+
+            public void Execute() {
+                var set = phase.Execute(parameterTables);
+                foreach(var phaseList in phaseMap.Values) {
+                    for(int i = 0; i < phaseList.Count; i++) {
+                        bool isError = false;
+                        phaseList[i].Operation(set.Tables[i], isError);
                     }
-                    // all iterators completed, run the operations for all conduits, but only once.
+                }
+                phase = null;
+                parameterTables = null;
+                phaseMap.Clear();
+            }
+
+            public PhaseT Chunk<PhaseT>(IConduit conduit, Action<DataRow> rowLoader, Action<DataTable, bool> operation) where PhaseT : IPhase, new() {
+                phase = phase ?? new PhaseT();
+                phase.Init(operation);
+                operation(new DataTable(), true);
+                parameterTables = parameterTables ?? phase.parameterTables;
+                if(!phaseMap.TryGetValue(phase.GetType(), out var list)) {
+                    phaseMap[phase.GetType()] = list = new List<IPhase>();
+                }
+                list.Add(phase);
+                foreach (var pair in parameterTables) {
+                    var row = pair.Value.NewRow();
+                    rowLoader(row);
+                    pair.Value.Rows.Add(row);
+                }
+                return (PhaseT)phase;
+            }
+        }
+        public interface IChannelState {
+            PhaseT Chunk<PhaseT>(IConduit conduit, Action<DataRow> value, Action<DataTable, bool> value1) where PhaseT : IPhase, new();
+        }
+        private struct ChannelItem {
+            public Action<IConduit> Operation;
+            public IEnumerator<IConduit> Enumerator;
+        }
+        private Dictionary<Type, ChannelState> _conduitMap = new Dictionary<Type, ChannelState>();
+
+        public bool IsOpen {
+            get {
+                return !_conduitMap.Values.All(x => !x.IsOpen);
+            }
+        }
+
+        public void Bind<ConduitT>(ref ConduitT conduitT, ref int id, ref Pipeline.IChannelState channelItem) {
+            var conduitType = typeof(ConduitT);
+            if (!_conduitMap.TryGetValue(conduitType, out var state)) {
+                throw new Exception($"Bind<{typeof(ConduitT).FullName}>() must only be called from {typeof(IConduit).FullName}'s {nameof(IConduit.Initialize)} method");
+            }
+            channelItem = state;
+            id = state.list.Count - 1;
+        }
+
+        public void Channel<IConduitT>(Func<IEnumerable<IConduit>> Origin, Action<IConduit> Operation) {
+            var conduitType = typeof(IConduitT);
+            if (!_conduitMap.TryGetValue(conduitType, out var state)) {
+                _conduitMap[conduitType] = state = new ChannelState();
+                state.list = new List<ChannelItem>();
+                state.IsOpen = false;
+                var enumerator =
+                state.IsChanneling = false;
+            }
+            state.list.Add(new ChannelItem() {
+                Operation = Operation,
+                Enumerator = Origin().GetEnumerator()
+            });
+        }
+
+        public void Clear() {
+            //throw new NotImplementedException();
+        }
+
+        public IEnumerable<IEnumerator<IConduit>> Flush<IConduitT>() {
+            var conduitType = typeof(IConduitT);
+            if (_conduitMap.TryGetValue(conduitType, out var state)) {
+                bool wasChanneling = state.IsChanneling;
+                if (!state.IsOpen) {
                     foreach (var item in state.list) {
-                        item.Operation(item.Enumerator.Current);
-
+                        if (!item.Enumerator.MoveNext()) {
+                            throw new Exception("Channel must receive a conduit that yields a non null value");
+                        }
+                        item.Enumerator.Current.Initialize(this);
                     }
+                    state.IsChanneling = true;
                 }
-                return state.list.Select(x => x.Enumerator);
-            }
+                while (state.IsChanneling) {
+                    foreach (var item in state.list) {
+                        var end = item.Enumerator.MoveNext();
+                        state.IsChanneling &= !end;
+                    }
+                    state.Execute();
+                    int index = 0;
+                    foreach (var item in state.list) {
+                        //item.Operation(state.parameterTables.First().Value.Rows[index++])
+                    }
+                    state.IsChanneling = !state.IsChanneling;
+                }
+                // all iterators completed, run the operations for all conduits, but only once.
+                foreach (var item in state.list) {
+                    item.Operation(item.Enumerator.Current);
 
-            public void GetChannelState<ConduitT>(ref bool IsOpen, ref bool IsChanneling) {
-                if (_conduitMap.TryGetValue(typeof(ConduitT), out var item)) {
-                    IsOpen = item.IsOpen;
-                    IsChanneling = item.IsChanneling;
-                } else {
-                    IsOpen = false;
-                    IsChanneling = false;
                 }
             }
-
+            return state.list.Select(x => x.Enumerator);
         }
+
+        public void GetChannelState<ConduitT>(ref bool IsOpen, ref bool IsChanneling) {
+            if (_conduitMap.TryGetValue(typeof(ConduitT), out var item)) {
+                IsOpen = item.IsOpen;
+                IsChanneling = item.IsChanneling;
+            } else {
+                IsOpen = false;
+                IsChanneling = false;
+            }
+        }
+
+    }
 }
